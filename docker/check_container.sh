37efc195977f491e91af62ad5e9122e179eb0d12
ductnn <trannhuduc531998@gmail.com>
2022-04-22 02:34:55 +0700

    feat: add script checking container

---
+++ docker/check_container.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+CONTAINER=$1
+
+if [ "x${CONTAINER}" == "x" ]; then
+  echo "UNKNOWN - Container ID or Friendly Name Required"
+  exit 3
+fi
+
+if [ "x$(which docker)" == "x" ]; then
+  echo "UNKNOWN - Missing docker binary"
+  exit 3
+fi
+
+docker info > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "UNKNOWN - Unable to talk to the docker daemon"
+  exit 3
+fi
+
+RUNNING=$(docker inspect --format="{{.State.Running}}" $CONTAINER 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+  echo "UNKNOWN - $CONTAINER does not exist."
+  exit 3
+fi
+
+if [ "$RUNNING" == "false" ]; then
+  echo "CRITICAL - $CONTAINER is not running."
+  exit 2
+fi
+
+RESTARTING=$(docker inspect --format="{{.State.Restarting}}" $CONTAINER)
+
+if [ "$RESTARTING" == "true" ]; then
+  echo "WARNING - $CONTAINER state is restarting."
+  exit 1
+fi
+
+STARTED=$(docker inspect --format="{{.State.StartedAt}}" $CONTAINER)
+NETWORK=$(docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $CONTAINER)
+
+echo "OK - $CONTAINER is running. IP: $NETWORK, StartedAt: $STARTED"
